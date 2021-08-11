3946cbcc79d989bf220107a5a2b61b6a414fcfed
Kiwi <kiwi@darae.dev>
2021-08-11 23:32:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,14 @@ if response.status_code == 200:
         title = notice.find("span",{"class":"list__title"}).get_text()
         date = notice.find("div",{"class":"list__date"}).get_text()
         if title.find("생성 가능 수치") != -1:
-            if date.find("분") == -1 and date.find("1시간") == -1 and date.find("2시간") == -1:
+            if date.find("분") == -1:
                 continue
             else:
                 bot = telegram.Bot(token=YOUR_BOT_TOKEN)
                 chat_id = YOUR_CHAT_ID
                 msg = date + " 캐릭터 생성 가능 수치 관련 공지가 올라온 것을 감지했습니다"
                 bot.sendMessage(chat_id=chat_id, text=msg)
+                break
 
 else : 
     print(response.status_code)
